651b32fae8b99d911a7ff33f364d899c6d361fed
JustNoob2005 <corne.jobse2005@gmail.com>
2023-10-20 00:26:39 +0200

    switch naar desktop

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,10 @@ class Product extends Model
     {
     	return $this->hasMany(Type::class);
     }
+    public function getPriceAttribute($value)
+    {
+        $discount = $value * ($this->discount / 100); // korting in euro's
+        $final_price = $value - $discount; // korting van prijs afhalen
+        return number_format($final_price, 2); // Zorg altijd voor 2 decimalen
+    }
 }
